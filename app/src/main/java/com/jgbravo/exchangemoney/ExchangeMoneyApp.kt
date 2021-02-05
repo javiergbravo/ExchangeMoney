51889ce89e91ddd9c34fec055d0e3fac64e8fc01
Javier Gutiérrez Bravo <jgbravo.developer@gmail.com>
2021-02-06 00:45:54 +0100

    First commit.

---
+++ app/src/main/java/com/jgbravo/exchangemoney/ExchangeMoneyApp.kt
@@ -0,0 +1,7 @@
+package com.jgbravo.exchangemoney
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ExchangeMoneyApp : Application()
---
+++ app/src/main/java/com/jgbravo/exchangemoney/ui/screens/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jgbravo.exchangemoney.ui.screens.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jgbravo.exchangemoney.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
